3ea3c87c408b15f4a38de23051942e64339e7c9f
gurleen84 <156744496+gurleen84@users.noreply.github.com>
2025-07-29 09:50:29 +0530

    Add files via upload

---
+++ bankingApp/bankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bankingApp
+//
+//  Created by Gurleen Singh on 19/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bankingApp/bankingAppTests/bankingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  bankingAppTests.swift
+//  bankingAppTests
+//
+//  Created by Gurleen Singh on 19/07/25.
+//
+
+import Testing
+@testable import bankingApp
+
+struct bankingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
