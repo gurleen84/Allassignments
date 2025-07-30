32bfbb9bcbbba1ef0e8e9b745544884651cf5a69
gurleen84 <156744496+gurleen84@users.noreply.github.com>
2025-07-30 11:31:21 +0530

    Add files via upload

---
+++ aboutMe/aboutMeTests/aboutMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeTests.swift
+//  aboutMeTests
+//
+//  Created by Gurleen Singh on 30/07/25.
+//
+
+import Testing
+@testable import aboutMe
+
+struct aboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
