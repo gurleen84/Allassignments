4bdd2a769fa38369662a9869a6b25db2a3a85fb0
gurleen84 <156744496+gurleen84@users.noreply.github.com>
2025-08-01 15:43:33 +0530

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Gurleen Singh on 25/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindToIntro(segue: UIStoryboardSegue){}
+
+
+}
+
